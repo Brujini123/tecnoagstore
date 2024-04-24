8714b794956a86447c4743bf082bcbee51024903
ErikRmz <147409948+Brujini123@users.noreply.github.com>
2024-04-24 11:01:00 -0600

    Add files via upload

---
+++ JS/funciones.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.querySelector('.navbar');
+    var body = document.querySelector('body');
+    var navbarHeight = navbar.offsetHeight;
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > navbarHeight) {
+            navbar.classList.add('fixed-top');
+            body.style.paddingTop = navbarHeight + 'px';
+        } else {
+            navbar.classList.remove('fixed-top');
+            body.style.paddingTop = '0';
+        }
+
+        document.getElementById('correo').addEventListener('click', function () {
+            window.location.href = 'mailto:erik.alj_ram@hotmail.com';
+        });
+    });
+});
